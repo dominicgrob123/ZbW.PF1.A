0de9df506cf9390216ead265548e589b027ba3b2
Dome <dominic.grob@hotmail.ch>
2024-11-01 14:13:23 +0100

    A1_2_5 added

---
+++ A1_2/A1_2_5.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace MB01.A1_2
+{
+    partial class A1_2_5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Cmd2to7 = new Button();
+            Cmd2to6 = new Button();
+            Cmd2to5 = new Button();
+            Cmd2to4 = new Button();
+            Cmd2to3 = new Button();
+            Cmd2to1 = new Button();
+            CmdDelete = new Button();
+            Cmd2to2 = new Button();
+            Cmd2to0 = new Button();
+            CmdBerechne = new Button();
+            LblWert = new Label();
+            SuspendLayout();
+            // 
+            // Cmd2to7
+            // 
+            Cmd2to7.Location = new Point(32, 23);
+            Cmd2to7.Name = "Cmd2to7";
+            Cmd2to7.Size = new Size(40, 40);
+            Cmd2to7.TabIndex = 0;
+            Cmd2to7.Text = "0";
+            Cmd2to7.UseVisualStyleBackColor = true;
+            Cmd2to7.Click += Cmd2to7_Click;
+            // 
+            // Cmd2to6
+            // 
+            Cmd2to6.Location = new Point(78, 23);
+            Cmd2to6.Name = "Cmd2to6";
+            Cmd2to6.Size = new Size(40, 40);
+            Cmd2to6.TabIndex = 0;
+            Cmd2to6.Text = "0";
+            Cmd2to6.UseVisualStyleBackColor = true;
+            Cmd2to6.Click += Cmd2to6_Click;
+            // 
+            // Cmd2to5
+            // 
+            Cmd2to5.Location = new Point(124, 23);
+            Cmd2to5.Name = "Cmd2to5";
+            Cmd2to5.Size = new Size(40, 40);
+            Cmd2to5.TabIndex = 0;
+            Cmd2to5.Text = "0";
+            Cmd2to5.UseVisualStyleBackColor = true;
+            Cmd2to5.Click += Cmd2to5_Click;
+            // 
+            // Cmd2to4
+            // 
+            Cmd2to4.Location = new Point(170, 23);
+            Cmd2to4.Name = "Cmd2to4";
+            Cmd2to4.Size = new Size(40, 40);
+            Cmd2to4.TabIndex = 0;
+            Cmd2to4.Text = "0";
+            Cmd2to4.UseVisualStyleBackColor = true;
+            Cmd2to4.Click += Cmd2to4_Click;
+            // 
+            // Cmd2to3
+            // 
+            Cmd2to3.Location = new Point(216, 23);
+            Cmd2to3.Name = "Cmd2to3";
+            Cmd2to3.Size = new Size(40, 40);
+            Cmd2to3.TabIndex = 0;
+            Cmd2to3.Text = "0";
+            Cmd2to3.UseVisualStyleBackColor = true;
+            Cmd2to3.Click += Cmd2to3_Click;
+            // 
+            // Cmd2to1
+            // 
+            Cmd2to1.Location = new Point(308, 23);
+            Cmd2to1.Name = "Cmd2to1";
+            Cmd2to1.Size = new Size(40, 40);
+            Cmd2to1.TabIndex = 0;
+            Cmd2to1.Text = "0";
+            Cmd2to1.UseVisualStyleBackColor = true;
+            Cmd2to1.Click += Cmd2to1_Click;
+            // 
+            // CmdDelete
+            // 
+            CmdDelete.Location = new Point(32, 107);
+            CmdDelete.Name = "CmdDelete";
+            CmdDelete.Size = new Size(362, 23);
+            CmdDelete.TabIndex = 0;
+            CmdDelete.Text = "Setze zurück";
+            CmdDelete.UseVisualStyleBackColor = true;
+            CmdDelete.Click += CmdDelete_Click;
+            // 
+            // Cmd2to2
+            // 
+            Cmd2to2.Location = new Point(262, 23);
+            Cmd2to2.Name = "Cmd2to2";
+            Cmd2to2.Size = new Size(40, 40);
+            Cmd2to2.TabIndex = 0;
+            Cmd2to2.Text = "0";
+            Cmd2to2.UseVisualStyleBackColor = true;
+            Cmd2to2.Click += Cmd2to2_Click;
+            // 
+            // Cmd2to0
+            // 
+            Cmd2to0.Location = new Point(354, 23);
+            Cmd2to0.Name = "Cmd2to0";
+            Cmd2to0.Size = new Size(40, 40);
+            Cmd2to0.TabIndex = 0;
+            Cmd2to0.Text = "0";
+            Cmd2to0.UseVisualStyleBackColor = true;
+            Cmd2to0.Click += Cmd2to0_Click;
+            // 
+            // CmdBerechne
+            // 
+            CmdBerechne.Location = new Point(32, 78);
+            CmdBerechne.Name = "CmdBerechne";
+            CmdBerechne.Size = new Size(362, 23);
+            CmdBerechne.TabIndex = 0;
+            CmdBerechne.Text = "Berechne Dezimalwert";
+            CmdBerechne.UseVisualStyleBackColor = true;
+            CmdBerechne.Click += CmdBerechne_Click;
+            // 
+            // LblWert
+            // 
+            LblWert.AutoSize = true;
+            LblWert.Location = new Point(32, 151);
+            LblWert.Name = "LblWert";
+            LblWert.Size = new Size(102, 15);
+            LblWert.TabIndex = 1;
+            LblWert.Text = "Wert in Dezimal: 0";
+            // 
+            // A1_2_5
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(422, 203);
+            Controls.Add(LblWert);
+            Controls.Add(Cmd2to0);
+            Controls.Add(Cmd2to2);
+            Controls.Add(Cmd2to1);
+            Controls.Add(Cmd2to3);
+            Controls.Add(CmdBerechne);
+            Controls.Add(CmdDelete);
+            Controls.Add(Cmd2to5);
+            Controls.Add(Cmd2to4);
+            Controls.Add(Cmd2to6);
+            Controls.Add(Cmd2to7);
+            Name = "A1_2_5";
+            Text = "A1_2_5";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button Cmd2to7;
+        private Button Cmd2to6;
+        private Button Cmd2to5;
+        private Button Cmd2to4;
+        private Button Cmd2to3;
+        private Button Cmd2to1;
+        private Button CmdDelete;
+        private Button Cmd2to2;
+        private Button Cmd2to0;
+        private Button CmdBerechne;
+        private Label LblWert;
+    }
+}
---
+++ A1_2/A1_2_5.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MB01.A1_2
+{
+    public partial class A1_2_5 : Form
+    {
+        public A1_2_5()
+        {
+            InitializeComponent();
+        }
+
+        private void Cmd2to7_Click(object sender, EventArgs e)
+        {
+            Cmd2to7.Text = "1";
+        }
+
+        private void Cmd2to6_Click(object sender, EventArgs e)
+        {
+            Cmd2to6.Text = "1";
+        }
+
+        private void Cmd2to5_Click(object sender, EventArgs e)
+        {
+            Cmd2to5.Text = "1";
+        }
+
+        private void Cmd2to4_Click(object sender, EventArgs e)
+        {
+            Cmd2to4.Text = "1";
+        }
+
+        private void Cmd2to3_Click(object sender, EventArgs e)
+        {
+            Cmd2to3.Text = "1";
+        }
+
+        private void Cmd2to2_Click(object sender, EventArgs e)
+        {
+            Cmd2to2.Text = "1";
+        }
+
+        private void Cmd2to1_Click(object sender, EventArgs e)
+        {
+            Cmd2to1.Text = "1";
+        }
+
+        private void Cmd2to0_Click(object sender, EventArgs e)
+        {
+            Cmd2to0.Text = "1";
+        }
+
+        private void CmdBerechne_Click(object sender, EventArgs e)
+        {
+            LblWert.Text = "Wert in Dezimal: " + Convert.ToString(Convert.ToInt16(Cmd2to7.Text + Cmd2to6.Text + Cmd2to5.Text + Cmd2to4.Text + Cmd2to3.Text + Cmd2to2.Text + Cmd2to1.Text + Cmd2to0.Text, 2));
+        }
+
+        private void CmdDelete_Click(object sender, EventArgs e)
+        {
+            Cmd2to0.Text = "0";
+            Cmd2to1.Text = "0";
+            Cmd2to2.Text = "0";
+            Cmd2to3.Text = "0";
+            Cmd2to4.Text = "0";
+            Cmd2to5.Text = "0";
+            Cmd2to6.Text = "0";
+            Cmd2to7.Text = "0";
+            LblWert.Text = "0";
+        }
+    }
+}
